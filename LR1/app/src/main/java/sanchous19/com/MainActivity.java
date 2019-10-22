4826d6e9d9abd0e7c9a3edd3ec80d29d3d1b5080
Sanchous19 <43664417+Sanchous19@users.noreply.github.com>
2019-10-22 03:16:52 +0300

    Added showing of version name

--- LR1/app/src/main/java/sanchous19/com/MainActivity.java
+++ LR1/app/src/main/java/sanchous19/com/MainActivity.java
@@ -1,14 +1,38 @@
 package sanchous19.com;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.DialogInterface;
+import android.view.View;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void showNeutralAlertDialog(String message) {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) { }
+            }
+        );
+        alertDialog.show();
+    }
+
+    public void showVersionNameClick(View view) {
+        String versionName = "Version name: " + BuildConfig.VERSION_NAME;
+        showNeutralAlertDialog(versionName);
+    }
+
+    public void showDeviceIdClick(View view) {
+        String versionName = "Device ID: ";
+        showNeutralAlertDialog(versionName);
     }
 }
